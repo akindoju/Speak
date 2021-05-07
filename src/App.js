c3cd08d6a4de666949c40d3ee9fd762c10f84766
Akndju <davidd.akindoju@gmail.com>
2021-05-07 08:39:47 +0100

    inserted default option

--- src/App.js
+++ src/App.js
@@ -1,64 +1,10 @@
-import { useState } from 'react';
 import './App.css';
+import Homepage from './Homepage';
 
 function App() {
-  const [textAreaText, setTextAreaText] = useState('');
-
-  let voices = [];
-  voices = speechSynthesis.getVoices();
-
-  const message = new SpeechSynthesisUtterance();
-
-  const speakText = () => {
-    speechSynthesis.speak(message);
-  };
-
-  const setTextMessage = (text) => {
-    message.text = text;
-  };
-
   return (
     <div className="App">
-      <div className="header">
-        <h1 className="headerTitle">Speak</h1>
-        <h3 className="headerTitleSub">Convert text to speech</h3>
-      </div>
-      <div className="textbox">
-        <h4 className="voiceSelect">Pick a Voice</h4>
-        <select
-          id="voices"
-          className="select"
-          onChange={(event) => {
-            message.voice = voices.find(
-              (voice) => voice.name === event.target.value
-            );
-          }}
-        >
-          {voices.map((voice) => {
-            return (
-              <option value={voice.name}>
-                {voice.name} &mdash; {voice.lang}
-              </option>
-            );
-          })}
-        </select>
-        <textarea
-          placeholder="Enter text to read..."
-          className="textArea"
-          onChange={(e) => {
-            e.preventDefault();
-            setTextAreaText(e.target.value);
-          }}
-        ></textarea>
-        <button
-          onClick={() => {
-            setTextMessage(textAreaText);
-            speakText();
-          }}
-        >
-          Read Text
-        </button>
-      </div>
+      <Homepage />
     </div>
   );
 }
---
+++ src/Homepage.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+const Homepage = () => {
+  const [textAreaText, setTextAreaText] = useState('');
+
+  const voices = speechSynthesis.getVoices();
+
+  const message = new SpeechSynthesisUtterance();
+
+  const setVoice = (event) => {
+    message.voice = voices.find((voice) => voice.name === event.target.value);
+  };
+
+  const speakText = () => {
+    speechSynthesis.speak(message);
+  };
+
+  const setTextMessage = (text) => {
+    message.text = text;
+  };
+
+  return (
+    <div>
+      <div className="header">
+        <h1 className="headerTitle">Speak</h1>
+        <h3 className="headerTitleSub">Convert text to speech</h3>
+      </div>
+      <div className="textbox">
+        <h4 className="voiceSelect">Pick a Voice</h4>
+        <select
+          className="select"
+          onChange={(event) => {
+            event.preventDefault();
+            setVoice(event);
+          }}
+        >
+          {/* <option key="default" value="Google US English en-US">
+            Default English
+          </option> */}
+          {voices.map((voice) => {
+            return (
+              <option key={voice.name} value={voice.name}>
+                {voice.name} {voice.lang}
+              </option>
+            );
+          })}
+        </select>
+        <textarea
+          placeholder="Enter text to read"
+          className="textArea"
+          onChange={(e) => {
+            e.preventDefault();
+            setTextAreaText(e.target.value);
+          }}
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setTextMessage(textAreaText);
+            speakText();
+          }}
+        >
+          Read Text
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
